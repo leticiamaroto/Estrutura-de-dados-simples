04a6b2166e245b45cadb6a87cf2ad1656f4a3f32
letícia <leticiamarotods01@outlook.com>
2022-02-22 17:01:39 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let frutas = ["Maça", "Banana", "Mamão", "Uva", "Abacaxi"]
+console.log(frutas)
+
+
+
+frutas.unshift("Ameixa")
+
+frutas.pop()
+
+frutas.push("Abacate", "Pêra")
+
+frutas.shift()
+
+
+
+let numbers = [7, 5, 6, 3, 8, 9, 2, 1, 4]
+console.log(numbers.sort())
